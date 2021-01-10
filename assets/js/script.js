783c8cb40a77a121797a617e4dc4f592a3fcef30
flokamp <74511633+flokamp@users.noreply.github.com>
2021-01-10 17:42:31 -0600

    hour array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 // The current day is displayed at the top of the calendar
 let DateTime = luxon.DateTime;
+var dt = DateTime.local();
+$("#currentDay").text(dt.toLocaleString(DateTime.DATE_HUGE));
 
-var dt = DateTime.local()
-$("#currentDay").text(dt.toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY))
+var hourArr = [];
 
-// show time blocks for standard business hours
-
-// color code time block to indicate past, present or future
-// enter event details on time block click
-// save event details to local storage
+$('.input-group').each(function() {
+  hourArr.push($(this).data('id'));
+})
+console.log(hourArr)
