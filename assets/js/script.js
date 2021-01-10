a8b8858b1d71a389f4450d96741e9b212ff2a206
flokamp <74511633+flokamp@users.noreply.github.com>
2021-01-10 15:37:58 -0600

    switched to luxon library

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 // The current day is displayed at the top of the calendar
-const today = dayjs().format('dddd, MMMM D, YYYY')
-$("#currentDay").text(today)
+let DateTime = luxon.DateTime;
+
+var dt = DateTime.local()
+$("#currentDay").text(dt.toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY))
 
 // show time blocks for standard business hours
+
 // color code time block to indicate past, present or future
 // enter event details on time block click
 // save event details to local storage
