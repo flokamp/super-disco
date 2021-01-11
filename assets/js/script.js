334e3182dec9f0d9d9c31699f2a0d8b0fa343885
flokamp <74511633+flokamp@users.noreply.github.com>
2021-01-10 18:44:38 -0600

    color coded time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,21 @@
 // The current day is displayed at the top of the calendar
 let DateTime = luxon.DateTime;
 var dt = DateTime.local();
-$("#currentDay").text(dt.toLocaleString(DateTime.DATE_HUGE));
+$("#currentDay").text(dt.toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY));
 
-var hourArr = [];
+// color code time block to indicate past, present or future
+$('.row').each(function() {
+  var taskHour = $(this).data('id');
+  var currentHour = DateTime.local().hour
+  
+  if (taskHour < currentHour) {
+    $(".form-control").addClass("past");
+  } else if (taskHour > currentHour) {
+    $(".form-control").addClass("future");
+  } else {
+    $(".form-control").addClass("present");
+  }
+});
 
-$('.input-group').each(function() {
-  hourArr.push($(this).data('id'));
-})
-console.log(hourArr)
+// enter event details on time block click
+// save event details to local storage 
